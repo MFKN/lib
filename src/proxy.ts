1da7e8dbe29d933101680fe103ab52a6b4aad27e
Banou <me@banou.dev>
2022-01-16 01:43:03 -0500

    chore: 🔥remove console log

--- src/proxy.ts
+++ src/proxy.ts
@@ -7,7 +7,6 @@ interface fetchInitOptions {
 
 export const fetch = async (input: RequestInfo, init?: RequestInit & fetchInitOptions) => {
   const { body, ...rest } = await call('FETCH', { input, init })
-  console.log('hmm', rest)
   return new Response(
     new Blob([new Uint8Array(body)]),
     {
