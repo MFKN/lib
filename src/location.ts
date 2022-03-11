e2d09fe3561973ad86d26a5506792d1b4fca3288
Banou <me@banou.dev>
2022-03-11 03:53:15 -0500

    refactor: ♻️remove useless location change code

--- src/location.ts
+++ src/location.ts
@@ -1,21 +1,6 @@
-import type { ApiResolverOptions } from '@mfkn/fkn-web'
-
-import makeApiPromise from './utils/promise'
 import { call } from './utils'
 
 window.addEventListener(
   'popstate',
   () => call('LOCATION_CHANGE', { url: location.toString() })
 )
-
-const locationChange = makeApiPromise(
-  async ({ data: { path } }: ApiResolverOptions<{ path: string }>) => {
-    console.log('app received nav', path)
-    history.pushState(undefined, '', path)
-  }
-)
-
-
-export const resolvers = {
-  'LOCATION_CHANGE': locationChange// ({ data: { path } }: { data: { path: string } }) => console.log('HISTORY PUSHED', path) || history.pushState(undefined, '', path)
-}
