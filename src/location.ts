d0870c32dff175141b3a807b30e3eb0ab4308c95
Banou <me@banou.dev>
2022-03-18 03:29:29 -0400

    feat: ✨take host popstate events and reapply them

--- src/location.ts
+++ src/location.ts
@@ -1,6 +1,21 @@
+import type { ApiResolverOptions } from '@mfkn/fkn-web'
+
 import { call } from './utils'
+import makeApiPromise from './utils/promise'
 
 window.addEventListener(
   'popstate',
   () => call('LOCATION_CHANGE', { url: location.toString() })
 )
+
+const locationChange = makeApiPromise(
+  async ({ data: { path } }: ApiResolverOptions<{ path: string }>) => {
+    history.pushState(undefined, '', path)
+    window.dispatchEvent(new Event('popstate'))
+  }
+)
+
+
+export const resolvers = {
+  'LOCATION_CHANGE': locationChange// ({ data: { path } }: { data: { path: string } }) => console.log('HISTORY PUSHED', path) || history.pushState(undefined, '', path)
+}
