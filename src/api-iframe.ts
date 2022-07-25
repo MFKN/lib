2798f31e961518c334ba5c2dc68fa83c5eb23bb3
Banou <me@banou.dev>
2022-07-25 02:11:49 +0200

    refactor: ♻️improve env var usage

--- src/api-iframe.ts
+++ src/api-iframe.ts
@@ -1,10 +1,14 @@
 
+const WEB_ORIGIN = process.env.WEB_ORIGIN
+
+if (!WEB_ORIGIN) throw new Error('Missing WEB_ORIGIN environment variable')
+
 export default (
   window.parent === window
     ? (
       document
         .body
-        .querySelector<HTMLIFrameElement>(`iframe[src="${`${process.env.WEB_ORIGIN}/sandbox-api`}"]`)!
+        .querySelector<HTMLIFrameElement>(`iframe[src="${`${WEB_ORIGIN}/sandbox-api`}"]`)!
     )
     : undefined
 )
