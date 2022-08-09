3a80a8cf16e65ecc8f3db43da1d3cce52c34302b
Banou26 <me@banou.dev>
2022-08-09 16:01:22 +0200

    fix: 🏷️improve proxy fetch typings

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,14 +1,16 @@
-import type { ProxyFetchRequestInit, FetchInitOptions } from '@mfkn/fkn-web/src/api/proxy/fetch'
+import type { ProxyFetchRequestInit, FetchInitOptions, ProxyFetchRequestInfo } from '@mfkn/fkn-web/src/api/proxy/fetch'
 
 import { call } from './utils/call'
-
 
 interface fetchInitOptions {
   proxyCache?: string
   proxyDelay?: string
 }
 
-export const fetch = async (input: RequestInfo, _init?: RequestInit & fetchInitOptions) => {
+type NativeFetch = typeof globalThis.fetch
+
+export const fetch = async (_input: Parameters<NativeFetch>[0], _init?: Parameters<NativeFetch>[1] & fetchInitOptions) => {
+  const input = _input as ProxyFetchRequestInfo
   const init = _init as ProxyFetchRequestInit & FetchInitOptions
   const { body, ...rest } = await call('FETCH', { input, init })
   return new Response(
